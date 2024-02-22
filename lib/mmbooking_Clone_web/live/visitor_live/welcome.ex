95ef001a41ba942cc3e25277d0410ee8e486037f
sangeetha <sangeethailango21@gmail.com>
2024-02-22 12:45:32 +0530

    checkbox is checked or not

--- lib/mmbooking_Clone_web/live/visitor_live/welcome.ex
+++ lib/mmbooking_Clone_web/live/visitor_live/welcome.ex
@@ -4,24 +4,37 @@ defmodule Mmbooking_CloneWeb.VisitorLive.Welcome do
   alias Mmbooking_Clone.User
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok,
+    socket
+    |> assign(agree: nil)
+    }
   end
 
   def handle_event("redirect", params, socket) do
-
     visitors = User.list_all_visitor()
-    emails =  Enum.map(visitors, fn visitor -> visitor.email_id end)
+    emails = Enum.map(visitors, fn visitor -> visitor.email_id end)
 
-    if params["email_id"] in emails do
-      {:noreply,
-      socket
-      |> push_navigate(to: ~p"/new_booking")
-      }
-    else
-      {:noreply,
-      socket
-      |> push_navigate(to: ~p"/new_visitor")
-      }
+    case params["agree"] == "true" do
+      true ->
+        case params["email_id"] in emails do
+          true ->
+          {:noreply,
+          socket
+          |> assign(agree: true)
+          |> push_navigate(to: "/new_booking")
+          }
+          false ->
+            {:noreply,
+            socket
+            |> assign(agree: true)
+            |> push_navigate(to: ~p"/new_visitor")
+            }
+        end
+      false ->
+        {:noreply,
+        socket
+        |> assign(agree: false)
+        }
     end
   end
 end
