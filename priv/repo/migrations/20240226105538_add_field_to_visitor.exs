16fda4dbc130deaaa059f692fb6fec02b8914deb
sangeetha <sangeethailango21@gmail.com>
2024-02-26 17:21:48 +0530

    Delete migration file

--- priv/repo/migrations/20240226105538_add_field_to_visitor.exs
+++
@@ -1,9 +0,0 @@
-defmodule Mmbooking_Clone.Repo.Migrations.AddFieldToUsers do
-  use Ecto.Migration
-
-  def change do
-    alter table("visitors") do
-      add :country_code, :string
-    end
-  end
-end
