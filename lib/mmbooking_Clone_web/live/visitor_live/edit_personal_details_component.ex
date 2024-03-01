698468b503a5325f80b657054bd9f8c9e9a18f22
sangeetha <sangeethailango21@gmail.com>
2024-03-01 10:23:26 +0530

    Align buttons

--- lib/mmbooking_Clone_web/live/visitor_live/edit_personal_details_component.ex
+++ lib/mmbooking_Clone_web/live/visitor_live/edit_personal_details_component.ex
@@ -5,15 +5,7 @@ defmodule Mmbooking_CloneWeb.VisitorLive.EditPersonalDetailsComponent do
   def render(assigns) do
     ~H"""
     <div>
-      <div class="grid grid-cols-2">
-        <p class="font-bold">Change Personal Details</p>
-        <div class="text-right">
-            <.link href={~p"/personal_details/#{@id}"}>
-              <.button>Cancel</.button>
-            </.link>
-        </div>
-      </div>
-
+      <p class="font-bold">Change Personal Details</p>
       <.simple_form :let={form} for={%{}} phx-target={@myself} phx-submit="submit">
         <div class="grid grid-cols-2 ">
           <p class="font-medium text-sm">Email ID</p>
@@ -25,7 +17,7 @@ defmodule Mmbooking_CloneWeb.VisitorLive.EditPersonalDetailsComponent do
         </div>
         <div class="grid grid-cols-2 ">
           <p class="font-medium text-sm">Date OF Birth</p>
-          <p><%= @dob %></p>
+          <p><%= Timex.format!(@dob, "{D}-{M}-{YYYY}") %></p>
         </div>
         <.input
           field={form[:last_name]}
@@ -51,10 +43,15 @@ defmodule Mmbooking_CloneWeb.VisitorLive.EditPersonalDetailsComponent do
           maxlength="30"
           required
           />
-          <div class="text-right ">
+          <div class=" flex justify-end gap-2   ">
+            <.link href={~p"/personal_details/#{@id}"}>
+              <p class="inline-block rounded-lg bg-zinc-900 hover:bg-zinc-700 py-2 px-3
+              text-sm font-semibold leading-6 text-white active:text-white/80 uppercase">Cancel</p>
+            </.link>
             <.button phx-disable-with="Saving..." value="submit">Save</.button>
           </div>
-      </.simple_form>
+
+       </.simple_form>
     </div>
     """
   end
